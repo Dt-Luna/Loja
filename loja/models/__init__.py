abdbf4b6d7a5b4c855c29ab1078cbd9c418920d7
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2026-04-24 10:32:12 -0300

    mais mudanças

---
+++ loja/models/__init__.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from ...loja.models.Fabricante import Fabricante
+from ...loja.models.Categoria import Categoria
+from ...loja.models.Produto import Produto
